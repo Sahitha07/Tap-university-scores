f2b5a6acd273834ddf1c63f2b515b49022c08cff
Sahitha07 <140141065+Sahitha07@users.noreply.github.com>
2023-07-25 15:33:18 +0900

    Update examinee.js

--- examinee.js
+++ examinee.js
@@ -1,60 +1,41 @@
-class Examinee {
-  constructor(division, english, math, science, japanese, geographyHistory) {
-    this.division = division;
-    this.english = english;
-    this.math = math;
-    this.science = science;
-    this.japanese = japanese;
-    this.geographyHistory = geographyHistory;
-  }
-
-  totalScore() {
-    return this.english + this.math + this.science + this.japanese + this.geographyHistory;
-  }
-
-  isScienceApplicant() {
-    return this.division === 's';
-  }
-
-  isHumanitiesApplicant() {
-    return this.division === 'l';
-  }
-
-  scienceScore() {
-    return this.math + this.science;
-  }
-
-  humanitiesScore() {
-    return this.japanese + this.geographyHistory;
-  }
-
-  hasHighScienceScores() {
-    return this.scienceScore() >= 160;
-  }
-
-  hasHighHumanitiesScores() {
-    return this.humanitiesScore() >= 160;
-  }
-
-  isPassed() {
-    const totalScore = this.totalScore();
-
-    if (totalScore < 350) {
-      return false;
+// Examinee class representing an individual examinee
+export class Examinee {
+    constructor(division, english, math, science, japanese, geographyHistory) {
+        // Initialize properties for each subject score and division
+        this.division = division;
+        this.english = english;
+        this.math = math;
+        this.science = science;
+        this.japanese = japanese;
+        this.geographyHistory = geographyHistory;
     }
-
-    if (this.isScienceApplicant()) {
-      return this.hasHighScienceScores();
+  
+    // Method to calculate and return the total score of the examinee
+    totalScore() {
+        return this.english + this.math + this.science + this.japanese + this.geographyHistory;
     }
-
-    if (this.isHumanitiesApplicant()) {
-      return this.hasHighHumanitiesScores();
+  
+    // Method to calculate and return the science score of the examinee
+    scienceScore() {
+        return this.math + this.science;
     }
-
-    return false;
-  }
+  
+    // Method to calculate and return the humanities score of the examinee
+    humanitiesScore() {
+        return this.japanese + this.geographyHistory;
+    }
+  
+    // Method to check if the examinee has passed the exam based on the total score and division
+    isPassed() {
+        // Calculate the total score, science score, and humanities score using class methods
+        const totalScore = this.totalScore();
+        const scienceScore = this.scienceScore();
+        const humanitiesScore = this.humanitiesScore();
+  
+        // Determine if the examinee has passed based on the total score and division
+        return totalScore >= 350 && ((this.division === 'l' && humanitiesScore >= 160) || (this.division === 's' && scienceScore >= 160));
+    }
 }
-
 
   
 
